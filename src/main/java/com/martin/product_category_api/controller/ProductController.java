5416d2372d121e53672f2ab653c3324a86468bb8
martinfiguerola <fedefiguerola@gmail.com>
2025-07-08 08:00:30 +0200

    feat(Product): Implement POST /product endpoint with DTO validations

---
+++ src/main/java/com/martin/product_category_api/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.martin.product_category_api.controller;
+
+import com.martin.product_category_api.dto.product.ProductRequestDTO;
+import com.martin.product_category_api.dto.product.ProductResponseDTO;
+import com.martin.product_category_api.service.product.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDTO> createProduct (@Valid @RequestBody ProductRequestDTO productRequestDTO) {
+        ProductResponseDTO responseDTO = productService.save(productRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+}
