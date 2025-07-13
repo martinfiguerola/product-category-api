c112d0e130aeb65e44c3854ab9d0fb81bc81ebb3
martinfiguerola <fedefiguerola@gmail.com>
2025-07-13 11:40:03 +0200

    feat: Establish one-to-many bidirectional relationship between Category and Product

--- src/main/java/com/martin/product_category_api/domain/Category.java
+++ src/main/java/com/martin/product_category_api/domain/Category.java
@@ -6,12 +6,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "categories")
 @Getter // Generates all getters
 @Setter // Generates all setters
 @NoArgsConstructor // Required by JPA: no-argument constructor
-@ToString // Generates toString(). Be careful with bidirectional relationships, consider excluding them.
+@ToString(exclude = {"products"}) // Generates toString(). Be careful with bidirectional relationships, consider excluding them.
 public class Category {
 
     @Id
@@ -19,6 +22,10 @@ public class Category {
     private Long id;
     private String name;
     private String description;
+    // @OneToMany annotation to specify the relationship between the Category entity and the Product entity
+    // "MappedBy" specifies the name of the attribute in the Product entity that owns the relationship
+    @OneToMany(mappedBy = "category") // This tells JPA that the Department entity is the inverse side (non-owning) of the relationship
+    private List<Product> products = new ArrayList<>();
 
 
 }
--- src/main/java/com/martin/product_category_api/domain/Product.java
+++ src/main/java/com/martin/product_category_api/domain/Product.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 @Getter // Generates all getters
 @Setter // Generates all setters
 @NoArgsConstructor // Required by JPA: no-argument constructor
-@ToString // Generates ToString(). Be careful with relationships, consider excluding them.
+@ToString(exclude = {"category"}) // Generates ToString(). Be careful with relationships, consider excluding them.
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +21,10 @@ public class Product {
     private String name;
     private String description;
     private BigDecimal price;
+    // @ManyToOne annotation to specify the relationship between the Product entity and the Category entity.
+    // @JoinColumn annotation specifies the name of the foreign key column in the Product table referencing the Category table.
+    // @ManyToOne along with @JoinColumn(name = "category_id"), indicates that the Product entity is the owning side of the relationship.
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
